e4a4d338e7269b49b477a60db38c90e8bad04908
MOHAMED-ALI-YOUSSOUF <wizzimed@gmail.com>
2024-10-01 00:06:12 +0300

    v2

--- src/app/firebaseConfig.js
+++ src/app/firebaseConfig.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3I9samkeM-QAGqZVJK6Dz04AM9in0yCk",
-  authDomain: "todoapp-f2d8b.firebaseapp.com",
-  projectId: "todoapp-f2d8b",
-  storageBucket: "todoapp-f2d8b.appspot.com",
-  messagingSenderId: "67439559056",
-  appId: "1:67439559056:web:c2f0c42c42ea30ad1a7315",
-  measurementId: "G-VPT1RGK2NS",
+ 
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
